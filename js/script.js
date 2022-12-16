9e754a375376cb3f786e21e4c856250558aef249
Daniele Minieri <danieleminieri99@gmail.com>
2022-12-16 15:35:36 +0100

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Richiesta nome
+const yourname = prompt ("Inserisci il tuo nome: ");
+
+// Richiesta Cognome
+const lastname = prompt ("Inserisci il tuo cognome: ");
+
+// Richiesta Colore Preferito
+const favouritecolor = prompt ("Inserisci il tuo colore preferito: ");
+
+// Output (Password a Video)
+document.getElementById("newpassword").innerHTML = yourname + lastname + favouritecolor + "21";
